271ba06256bdf8bf3fa51ba3fa0e581c345139da
zeinabrika <fatemi.zeinab@gmail.com>
2021-09-20 14:04:38 +0100

    PEW-44 String translations

--- wp-content/themes/castrads-theme/footer.php
+++ wp-content/themes/castrads-theme/footer.php
@@ -4,38 +4,38 @@
         <div id="left-footer">
 
             <div class="pr-2">
-                <h3><?php _e('Quick Links'); ?></h3>
+                <h3><?php _e('Quick Links', 'Castrads'); ?></h3>
 
                 <ul>
                     <li>
-                        <a href="<?php echo site_url('/products'); ?>"><?php _e('Products'); ?></a>
+                        <a href="<?php echo site_url('/products'); ?>"><?php _e('Products', 'Castrads'); ?></a>
                     </li>
                     <li>
-                        <a href="<?php echo site_url('/colorsamples'); ?>"><?php _e('Color Samples'); ?></a>
+                        <a href="<?php echo site_url('/colorsamples'); ?>"><?php _e('Color Samples', 'Castrads'); ?></a>
                     </li>
                     <li>
-                        <a href="<?php echo site_url('/resources'); ?>"><?php _e('Resources'); ?></a>
+                        <a href="<?php echo site_url('/resources'); ?>"><?php _e('Resources', 'Castrads'); ?></a>
                     </li>
                     <li>
-                        <a href="<?php echo site_url('/inspiration'); ?>"><?php _e('Inspiration'); ?></a>
+                        <a href="<?php echo site_url('/inspiration'); ?>"><?php _e('Inspiration', 'Castrads'); ?></a>
                     </li>
                     <li>
-                        <a href="<?php echo site_url('/ourstory'); ?>"><?php _e('Our Story'); ?></a>
+                        <a href="<?php echo site_url('/ourstory'); ?>"><?php _e('Our Story', 'Castrads'); ?></a>
                     </li>
                     <li>
-                        <a href="<?php echo site_url('/contact'); ?>"><?php _e('Contact'); ?></a>
+                        <a href="<?php echo site_url('/contact'); ?>"><?php _e('Contact', 'Castrads'); ?></a>
                     </li>
                 </ul>
             </div>
 
             <div id="mid-footer">
-                <h3><?php _e('Other Branches'); ?></h3>
+                <h3><?php _e('Other Branches', 'Castrads'); ?></h3>
                 <ul>
                     <li>
-                        <a href="https://www.castrads.com/uk/" target="_blank" rel="noopener"><?php _e('United Kingdom'); ?></a>
+                        <a href="https://www.castrads.com/uk/" target="_blank" rel="noopener"><?php _e('United Kingdom', 'Castrads'); ?></a>
                     </li>
                     <li>
-                        <a href="https://www.castrads.com/us/" target="_blank" rel="noopener"><?php _e('United State'); ?></a>
+                        <a href="https://www.castrads.com/us/" target="_blank" rel="noopener"><?php _e('United State', 'Castrads'); ?></a>
                     </li>
                 </ul>
             </div>
@@ -43,17 +43,8 @@
 
         </div>
 
-
-
-
-
-
-
-
-
-
         <div id="right-footer">
-            <h3><?php _e('Follow us on'); ?></h3>
+            <h3><?php _e('Follow us on', 'Castrads'); ?></h3>
             <div id="social-media-footer">
                 <ul>
                     <li>
